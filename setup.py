cd88f5c13ae9a0596f3858e574928fa82fb54c36
JUNAID <moinuddinm942@gmail.com>
2025-09-14 22:51:14 +0530

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="agentic ai using langgraph",
+    version="0.1",
+    author="MOINUDDIN",
+    author_email="moinuddinm942@gmail.com",
+    packages=find_packages(),
+    installrequires=[]
+)
